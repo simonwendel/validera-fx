b5237d62597eb8389410b0634fda6144c650c20d
Simon Wendel <mail@simonwendel.se>
2025-07-21 21:28:41 +0200

    chore: add licenses to files missing them

--- ValideraFx.Core.Tests/ValidatorServiceBuilderTests.cs
+++ ValideraFx.Core.Tests/ValidatorServiceBuilderTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿// SPDX-FileCopyrightText: 2025 Simon Wendel
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+using System.Reflection;
 using FluentAssertions;
 using ValideraFx.Core.Validators;
 
--- ValideraFx.Core/ValidatorServiceBuilder.cs
+++ ValideraFx.Core/ValidatorServiceBuilder.cs
@@ -1,4 +1,7 @@
-﻿namespace ValideraFx.Core;
+﻿// SPDX-FileCopyrightText: 2025 Simon Wendel
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+namespace ValideraFx.Core;
 
 public class ValidatorServiceBuilder
 {
