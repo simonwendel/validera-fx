e7f344e27d8bea87dd644759ea3d73add73b665d
Simon Wendel <mail@simonwendel.se>
2025-07-21 16:16:24 +0200

    refactor: merge complementary IValidator interfaces

--- ValideraFx.Core/IValidator.cs
+++ ValideraFx.Core/IValidator.cs
@@ -3,6 +3,12 @@
 
 namespace ValideraFx.Core;
 
+public interface IValidator<T> where T : notnull
+{
+    T Validate(UntrustedValue<T> untrustedValue);
+}
+
+
 public interface IValidator
 {
     T Validate<T>(UntrustedValue<T> untrustedValue) where T : notnull;
--- ValideraFx.Core/Validators/IValidatorBase.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Simon Wendel
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-namespace ValideraFx.Core.Validators;
-
-internal interface IValidatorBase<T> where T : notnull
-{
-    T Validate(UntrustedValue<T> untrustedValue);
-}
