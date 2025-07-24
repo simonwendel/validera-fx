02c6793cf98b0f0c927ac295d36ec4d02ad9e896
Simon Wendel <mail@simonwendel.se>
2025-07-24 10:20:58 +0200

    feat: add an ASP.NET Core UntrustedValue<T> model binder

---
+++ ValideraFx.Web.Tests/UntrustedValueBinderProviderTests.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-FileCopyrightText: 2025 Simon Wendel
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+using FluentAssertions;
+
+namespace ValideraFx.Web.Tests;
+
+public class UntrustedValueBinderProviderTests
+{
+    [Fact]
+    public void GetBinder_GivenNullContext_ThrowsException()
+    {
+        var provider = new UntrustedValueBinderProvider();
+        Action getting = () => provider.GetBinder(null);
+        getting.Should().Throw<ArgumentNullException>().Which.ParamName.Should().Be("context");
+    }
+}
---
+++ ValideraFx.Web/UntrustedValueBinder.cs
@@ -0,0 +1,42 @@
+﻿// SPDX-FileCopyrightText: 2025 Simon Wendel
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ValideraFx.Core;
+
+namespace ValideraFx.Web;
+
+internal class UntrustedValueBinder(
+    Type innerType,
+    IModelMetadataProvider metadataProvider,
+    IModelBinderFactory binderFactory) : IModelBinder
+{
+    public async Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        var innerMetadata = metadataProvider.GetMetadataForType(innerType);
+        var binder = binderFactory.CreateBinder(new ModelBinderFactoryContext
+        {
+            Metadata = innerMetadata,
+            BindingInfo = null,
+            CacheToken = innerType
+        });
+
+        var nestedContext = DefaultModelBindingContext.CreateBindingContext(
+            bindingContext.ActionContext,
+            bindingContext.ValueProvider,
+            innerMetadata,
+            bindingInfo: null,
+            modelName: bindingContext.ModelName);
+
+        await binder.BindModelAsync(nestedContext);
+
+        if (nestedContext.Result.IsModelSet)
+        {
+            var genericDef = typeof(UntrustedValue<>);
+            var closedType = genericDef.MakeGenericType(innerType);
+            var instance = Activator.CreateInstance(closedType, nestedContext.Result.Model);
+            bindingContext.ModelState.Clear();
+            bindingContext.Result = ModelBindingResult.Success(instance);
+        }
+    }
+}
