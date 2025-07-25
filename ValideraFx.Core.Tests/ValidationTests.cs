89fefa31d50ed8a059bbac79bccb0960ee502045
Simon Wendel <mail@simonwendel.se>
2025-07-25 13:46:06 +0200

    feat: add sanity check in validators when building

---
+++ ValideraFx.Core.Tests/ValidationTests.cs
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: 2025 Simon Wendel
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using ValideraFx.Core.Validators;
+
+namespace ValideraFx.Core.Tests;
+
+public class ValidationTests
+{
+    [Fact]
+    public void Apply_GivenNonMemberSelectorLambda_ThrowsException()
+    {
+        Action applying = () => Validation.Of<SomeType>()
+            .Apply(x => x.Value1 + 1, Limit.AtLeast(0));
+
+        applying.Should().Throw<ArgumentException>()
+            .WithMessage("The selector expression must be a member access expression, but was: (x.Value1 + 1)");
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Build_WhenNoValidatorsAdded_ThrowsException(bool allowMissing)
+    {
+        Action building = () => Validation.Of<SomeType>().Build(allowMissing);
+        building.Should().Throw<InvalidOperationException>()
+            .WithMessage("No validators have been added to the validation builder.");
+    }
+
+    [Fact]
+    public void Build_GivenAllowMissingFalseAndMissingProperty_ThrowsException()
+    {
+        Action building = () => Validation.Of<SomeType>()
+            .Apply(x => x.Value1, Limit.AtLeast(0))
+            .Build(allowMissing: false);
+
+        building.Should().Throw<InvalidOperationException>()
+            .WithMessage("Not all properties/fields of SomeType are validated. Missing: Value2, value3");
+    }
+
+    [Fact]
+    public void Build_GivenAllowMissingFalseAndNoMissingProperties_ReturnsValidator()
+    {
+        var validator = Validation.Of<SomeType>()
+            .Apply(x => x.Value1, Limit.AtLeast(0))
+            .Apply(x => x.Value2, Limit.AtLeast(0))
+            .Apply(x => x.value3, Limit.AtLeast(0))
+            .Build(allowMissing: false);
+
+        validator.Should().NotBeNull();
+        validator.Should().BeOfType<Pipeline<SomeType>>();
+    }
+
+    [Fact]
+    public void Build_GivenAllowMissingTrueAndMissingProperty_ReturnsValidator()
+    {
+        var validator = Validation.Of<SomeType>()
+            .Apply(x => x.Value1, Limit.AtLeast(0))
+            .Build(allowMissing: true);
+
+        validator.Should().NotBeNull();
+        validator.Should().BeOfType<Pipeline<SomeType>>();
+    }
+
+    [ExcludeFromCodeCoverage]
+    private class SomeType
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public int value3;
+        
+        public void SetValue3(int value)
+        {
+            value3 = value;
+        }
+    }
+}
--- ValideraFx.Core/Validation.cs
+++ ValideraFx.Core/Validation.cs
@@ -1,6 +1,8 @@
 ﻿// SPDX-FileCopyrightText: 2025 Simon Wendel
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
+using System.Linq.Expressions;
+using System.Reflection;
 using ValideraFx.Core.Validators;
 
 namespace ValideraFx.Core;
@@ -16,17 +18,68 @@ public static class Validation
 public class Validation<T> where T : notnull
 {
     private readonly List<Validator<T>> validatorPipeline = [];
-    
-    public Validation<T> Apply<TProp>(System.Linq.Expressions.Expression<Func<T, TProp>> selector,
+    private readonly List<MemberExpression> memberSelectors = [];
+
+    public Validation<T> Apply<TProp>(Expression<Func<T, TProp>> selector,
         params IValidator<TProp>[] validators) where TProp : notnull
     {
-        var newValidators = validators.Select(v => new ObjectPropertyValidator<T, TProp>(selector, v));
-        validatorPipeline.AddRange(newValidators);
+        SaveSelectorForLater(selector);
+        AddNewValidators(selector, validators);
         return this;
     }
 
-    public IValidator<T> Build()
+    public IValidator<T> Build(bool allowMissing = false)
     {
+        if (validatorPipeline.Count == 0)
+        {
+            throw new InvalidOperationException("No validators have been added to the validation builder.");
+        }
+
+        if (!allowMissing)
+        {
+            EnsureTypeIsProperlyValidated();
+        }
+
         return new Pipeline<T>(validatorPipeline.ToArray());
+    }
+
+    private void SaveSelectorForLater<TProp>(Expression<Func<T, TProp>> selector)
+    {
+        if ((selector.Body is UnaryExpression u ? u.Operand : selector.Body) is not MemberExpression memberExpression)
+        {
+            throw new ArgumentException(
+                $"The selector expression must be a member access expression, but was: {selector.Body}");
+        }
+
+        memberSelectors.Add(memberExpression);
+    }
+
+    private void AddNewValidators<TProp>(Expression<Func<T, TProp>> selector, IValidator<TProp>[] validators)
+        where TProp : notnull
+    {
+        var newValidators = validators.Select(v => new ObjectPropertyValidator<T, TProp>(selector, v));
+        validatorPipeline.AddRange(newValidators);
+    }
+
+    private void EnsureTypeIsProperlyValidated()
+    {
+        var missingProperties = GetMissingMembers();
+        if (missingProperties.Count > 0)
+        {
+            throw new InvalidOperationException(
+                $"Not all properties/fields of {typeof(T).Name} are validated. Missing: {string.Join(", ", missingProperties)}");
+        }
+    }
+
+    private List<string> GetMissingMembers()
+    {
+        var selectedNames = memberSelectors.Select(x => x.Member.Name).ToHashSet();
+
+        const BindingFlags attributes = BindingFlags.Instance | BindingFlags.Public;
+        var allProps = typeof(T).GetProperties(attributes).Select(x => x.Name);
+        var allFields = typeof(T).GetFields(attributes).Select(x => x.Name);
+
+        var allMembers = allProps.Concat(allFields);
+        return allMembers.Where(x => !selectedNames.Contains(x)).OrderBy(x => x).ToList();
     }
 }
