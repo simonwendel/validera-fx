90f59b3bfb75a445a42bd00b9a1dc54b05ca1dec
Simon Wendel <mail@simonwendel.se>
2025-07-26 11:03:28 +0200

    style: make Limit more terse with expression bodies

--- ValideraFx.Core/Limit.cs
+++ ValideraFx.Core/Limit.cs
@@ -7,38 +7,17 @@ namespace ValideraFx.Core;
 
 public static class Limit
 {
-    public static IValidator<string> Length(int min, int max = int.MaxValue)
-    {
-        return new StringLengthValidator(min, max);
-    }
+    public static IValidator<string> Length(int min, int max = int.MaxValue) => new StringLengthValidator(min, max);
 
-    public static IValidator<int> Between(int min, int max)
-    {
-        return new IntegerIntervalValidator(min, max);
-    }
+    public static IValidator<int> Between(int min, int max) => new IntegerIntervalValidator(min, max);
 
-    public static IValidator<int> AtLeast(int min)
-    {
-        return new IntegerIntervalValidator(min);
-    }
+    public static IValidator<int> AtLeast(int min) => new IntegerIntervalValidator(min);
 
-    public static IValidator<int> AtMost(int max)
-    {
-        return new IntegerIntervalValidator(int.MinValue, max);
-    }
-    
-    public static IValidator<string> ToBase64()
-    {
-        return new Base64AlphabetValidator();
-    }
-    
-    public static IValidator<string> ToAlphaNumericAscii()
-    {
-        return new AsciiAlphaNumericStringValidator();
-    }
+    public static IValidator<int> AtMost(int max) => new IntegerIntervalValidator(int.MinValue, max);
 
-    public static IValidator<string> ToNonEmptyString()
-    {
-        return new NonEmptyStringValidator();
-    }
+    public static IValidator<string> ToBase64() => new Base64AlphabetValidator();
+
+    public static IValidator<string> ToAlphaNumericAscii() => new AsciiAlphaNumericStringValidator();
+
+    public static IValidator<string> ToNonEmptyString() => new NonEmptyStringValidator();
 }
