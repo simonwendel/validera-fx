eac9652461e8f88ceb63cad417fb63346f096945
Simon Wendel <mail@simonwendel.se>
2025-07-27 10:54:31 +0200

    feat: add a nop validator that simply passes an object through

---
+++ ValideraFx.Core.Tests/Validators/NoOpValidatorTests.cs
@@ -0,0 +1,18 @@
+﻿// SPDX-FileCopyrightText: 2025 Simon Wendel
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using ValideraFx.Core.Validators;
+
+namespace ValideraFx.Core.Tests.Validators;
+
+public class NoOpValidatorTests
+{
+    [Theory, AutoData]
+    internal void Validate_Always_ReturnsValue(object obj, NoOpValidator<object> sut)
+    {
+        var untrusted = new UntrustedValue<object>(obj);
+        sut.Validate(untrusted).Should().Be(obj);
+    }
+}
