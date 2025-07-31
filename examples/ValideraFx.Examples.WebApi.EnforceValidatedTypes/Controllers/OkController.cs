9cd957b287c30362fcf13b6d080a6f22bf78b981
Simon Wendel <mail@simonwendel.se>
2025-07-31 22:31:09 +0200

    chore: exclude controller from coverage, throws on startup

--- examples/ValideraFx.Examples.WebApi.EnforceValidatedTypes/Controllers/OkController.cs
+++ examples/ValideraFx.Examples.WebApi.EnforceValidatedTypes/Controllers/OkController.cs
@@ -1,10 +1,12 @@
 // SPDX-FileCopyrightText: 2025 Simon Wendel
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ValideraFx.Examples.WebApi.EnforceValidatedTypes.Controllers;
 
+[ExcludeFromCodeCoverage]
 [ApiController]
 [Route("[controller]")]
 public class OkController : ControllerBase
