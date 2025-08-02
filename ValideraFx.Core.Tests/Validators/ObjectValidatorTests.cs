bf5428d6c7d34384cc7ac741a93b97e481c3204e
Simon Wendel <mail@simonwendel.se>
2025-08-02 09:54:16 +0200

    chore: remove unused namespace using statement

--- ValideraFx.Core.Tests/Validators/ObjectValidatorTests.cs
+++ ValideraFx.Core.Tests/Validators/ObjectValidatorTests.cs
@@ -1,7 +1,6 @@
 ﻿// SPDX-FileCopyrightText: 2025 Simon Wendel
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-using AutoFixture.Xunit2;
 using FluentAssertions;
 using ValideraFx.Core.Validators;
 
